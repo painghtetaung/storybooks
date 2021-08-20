560ca0c8b7cb47abbdd9217ea2fcdf79c1a57dd2
painghtetaung <painghtetaung1999@gmail.com>
2021-08-20 13:32:26 +0630

    added logout route and navbar

--- routes/auth.js
+++ routes/auth.js
@@ -9,8 +9,16 @@ router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 // @desc Google auth callback
 // @route GET /auh/google/callback
 router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }),
-        (req, res) => {
-            res.redirect('/dashboard')
-        })
+    (req, res) => {
+        res.redirect('/dashboard')
+    })
+
+
+// @desc Logout User
+// @route /auth/logout
+router.get('/logout', (req, res) => {
+        req.logout()
+        res.redirect('/')
+    })
     //then require in app.js
 module.exports = router
