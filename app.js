c1a4a241a30f4e650f597bddba027cdf4d20e573
painghtetaung <painghtetaung1999@gmail.com>
2021-09-02 13:18:46 +0630

    add stories to dashboard && format date

--- app.js
+++ app.js
@@ -19,13 +19,20 @@ connectDB()
 
 const app = express()
 
+// Body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
 //logging
 if (process.env.NODE_ENV === "development") {
     app.use(morgan('dev'))
 }
 
-// Handler
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+// Handlebars Helpers
+const { formatDate } = require('./helpers/hbs')
+
+// Handlebars
+app.engine('.hbs', exphbs({ helpers: { formatDate }, defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
 // Sessions
@@ -50,7 +57,9 @@ app.use(express.static(path.join(__dirname, 'public')))
 //routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
-    //
+app.use('/stories', require('./routes/stories'))
+
+//
 const PORT = process.env.PORT || 3000
 
 app.listen(
